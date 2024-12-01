1684f9146576d8b94abcd70d43b346e40a096f6c
rsaissi.mohamed <rsaissi.mohamed@telecom-st-etienne.fr>
2024-12-01 22:43:03 +0100

    setup/adding classes for sprint 2

---
+++ code/src/main/java/com/project/model/Character.java
@@ -0,0 +1,62 @@
+package com.project.model;
+
+
+public class Character {
+    private int id; // Unique identifier for the character
+    private String name; // Character's name (e.g., "Batman")
+    private String realName; // Real name of the character (e.g., "Bruce Wayne")
+    private String imageUrl; // URL of the character's image
+    private String description; // Description of the character
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {""
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", realName='" + realName + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
