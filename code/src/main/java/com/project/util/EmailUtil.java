0a6c778a8529da40bc3d942200b3e4598d9cd84c
rsaissi.mohamed <rsaissi.mohamed@telecom-st-etienne.fr>
2024-12-19 11:49:57 +0100

    feat: add EmailUtil class for sending emails using Gmail SMTP

---
+++ code/src/main/java/com/project/util/EmailUtil.java
@@ -0,0 +1,37 @@
+package com.project.util;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class EmailUtil {
+    public static void sendEmail(String to, String subject, String content) {
+        final String username = "archivage2203@gmail.com";
+        final String password = "pwfs ohdy jhmx xvxx";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(content);
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
